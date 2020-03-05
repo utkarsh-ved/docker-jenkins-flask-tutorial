5b7e59f025641153dbec4d46b32b641d1cdd2af8
utkarsh-ved <61776633+utkarsh-ved@users.noreply.github.com>
2020-03-05 17:33:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -22,4 +22,4 @@ def predict():
 	return jsonify(predict[0].tolist())
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5001)
+    app.run(debug=True,host='172.19.42.20', port=5002)
