f2ebe1b7a9cf43b31324d178bab62c47b0e4da76
utkarsh-ved <61776633+utkarsh-ved@users.noreply.github.com>
2020-03-05 17:35:59 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 # Change the value of experience that you want to test
-url = 'http://172.19.42.20:5002/api'
+url = 'http://0.0.0.0:5002/api'
 feature = [[5.8, 4.0, 1.2, 0.2]]
 labels ={
   0: "setosa",
